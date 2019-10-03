fef01418e65edf59fc08ee260909f4f2e100bee2
j.linhart <linhartos@centrum.cz>
2019-10-03 17:06:39 +0200

    RFTR - out of bounds

--- src/main/kotlin/cz/jlinhart/barcamp/Line.kt
+++ src/main/kotlin/cz/jlinhart/barcamp/Line.kt
@@ -9,8 +9,12 @@ object NoLine : MaybeLine()
 class Line(val start: Point, val end: Point, val color: Color) : Drawable, MaybeLine() {
 
     override fun draw(c: Canvas) {
+        c.plot(start)
+    }
+
+    private fun Canvas.plot(point: Point) {
         try {
-            c.pixel(start, color)
+            pixel(point, color)
         } catch (e: ArrayIndexOutOfBoundsException) {
             // no problem - just drawing outside of grid
         }
--- src/test/kotlin/cz/jlinhart/barcamp/LineTest.kt
+++ src/test/kotlin/cz/jlinhart/barcamp/LineTest.kt
@@ -1,26 +1,43 @@
 package cz.jlinhart.barcamp
 
 import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doThrow
 import com.nhaarman.mockitokotlin2.eq
-import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.given
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
 import java.awt.Color
 
+@ExtendWith(MockitoExtension::class)
 internal class LineTest {
+
+    @Mock
+    private lateinit var canvas: Canvas
+
+    private val color = Color.WHITE
 
     @Test
     fun `drawing point outside of grid does not throw`() {
         // given
-        val p = Point(4, -1)
-        val line = Line(p, p, Color.WHITE)
-        val canvas = mock<Canvas> { on { pixel(eq(p), any()) } doThrow ArrayIndexOutOfBoundsException() }
+        val p = 4 to -1
+        givenPointIsOutOfBounds(p)
 
         // when
-        val action = { line.draw(canvas) }
+        val action = { drawPoint(p) }
 
         // then
         assertDoesNotThrow(action)
     }
+
+    private fun givenPointIsOutOfBounds(p: Point) {
+        given(canvas.pixel(eq(p), any())).willThrow(ArrayIndexOutOfBoundsException())
+    }
+
+    private fun drawPoint(p: Point) {
+        Line(p, p, color).draw(canvas)
+    }
+
+    private infix fun Int.to(y: Int): Point = Point(this, y)
 }
