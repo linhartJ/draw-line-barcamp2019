87b7c6887a9782e304e7956691a86340433c3049
j.linhart <linhartos@centrum.cz>
2019-10-03 16:55:27 +0200

    TEST - out of bounds

--- src/test/kotlin/cz/jlinhart/barcamp/LineTest.kt
+++ src/test/kotlin/cz/jlinhart/barcamp/LineTest.kt
@@ -1,5 +1,26 @@
 package cz.jlinhart.barcamp
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doThrow
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import java.awt.Color
+
 internal class LineTest {
 
+    @Test
+    fun `drawing point outside of grid does not throw`() {
+        // given
+        val p = Point(4, -1)
+        val line = Line(p, p, Color.WHITE)
+        val canvas = mock<Canvas> { on { pixel(eq(p), any()) } doThrow ArrayIndexOutOfBoundsException() }
+
+        // when
+        val action = { line.draw(canvas) }
+
+        // then
+        assertDoesNotThrow(action)
+    }
 }
